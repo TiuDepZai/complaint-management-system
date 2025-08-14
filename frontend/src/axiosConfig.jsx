3d695ed86f72e2a1a6b7d1b585c36914ae0d8ba1
ahmadsaif24 <60265031+ahmadsaif24@users.noreply.github.com>
2025-08-14 10:35:18 +1000

    changed in testing and workflows

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local commit check again
-  // baseURL: 'http://3.25.144.206:5001', // live changed once again for another commit once again
+  baseURL: 'http://3.26.65.33:5001', // live changed once again for another commit once again
   headers: { 'Content-Type': 'application/json' },
 });
 
