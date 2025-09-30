83b66f44a0114a138626b3089c8d0c9e10889c0a
TiuDepZai <58964817+TiuDepZai@users.noreply.github.com>
2025-09-30 23:21:07 +1000

    Singletone Implementation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
-// config/db.js
 const mongoose = require("mongoose");
 
-// Set strictQuery explicitly to suppress the warning
-//mongoose.set('strictQuery', true);
+let connection = null; 
 
 const connectDB = async () => {
+  if (connection) {
+    return connection;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
+    connection = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
+    return connection;
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
