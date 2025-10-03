b264d198b253bdb569b0e00f8ee3d6f45591018a
TiuDepZai <58964817+TiuDepZai@users.noreply.github.com>
2025-10-03 18:31:27 +1000

    Yml and ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local commit check
-  // baseURL: 'http://54.79.147.114:5001', // live changed once again for another commit once again
+  // baseURL: 'http://localhost:5001', // local commit check
+  baseURL: 'http://13.211.162.126:5001', // live changed once again for another commit once again
   headers: { 'Content-Type': 'application/json' },
 });
 
